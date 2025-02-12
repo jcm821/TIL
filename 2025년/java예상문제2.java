5d3545f1937af93bcee2f07825948657ab8620bb
Changmin Jeon <jchangmin718@gmail.com>
2025-02-12 15:49:42 +0900

    Today I learned

---
+++ 2025년/java예상문제2.java
@@ -0,0 +1,19 @@
+class Book{
+    String name = '수험서';
+    void getPrice(String name){
+        System.out.print('name : ' + name);
+    }
+}
+class Price extends Book{
+    String price = '10,000원';
+    void getPrice(String name){
+        super.getPrice(name);
+        System.out.print('price : ' + price);
+    }
+}
+class Soojebi{
+    public static void main(String[] args){
+        Book b = new Price();
+        b.getPrice('정보처리기사');
+    }
+}
